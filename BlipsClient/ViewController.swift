40b5539861a455a25be3400050390feb27f9137d
Cyrus Sadeghi <cyrus.cse@gmail.com>
2017-10-29 17:36:38 -0400

    Make static JSON request to Blips server, gets a returned object (which can't be parsed yet, Blips server needs to JSONify its output)

--- BlipsClient/ViewController.swift
+++ BlipsClient/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    let regionRadius: CLLocationDistance = 10
+    
+    let jsonRequest = ["cityID": "1", "type": "lodging"]
+    let session = URLSession.shared
+    let url:URL = URL(string: "http://172.16.0.2:3000")!
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func postBlipsServer() {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: jsonRequest, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+            
+            return
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler : { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                print(data)
+            }
+            /*catch let error {
+                print(error.localizedDescription)
+            }*/
+        })
+        
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        centerMapOnLocation(location: initialLocation)
+        postBlipsServer()
     }
 
     override func didReceiveMemoryWarning() {
